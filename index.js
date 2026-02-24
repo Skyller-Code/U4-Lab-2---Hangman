58a1247d7d5086b6e10ce016cb31ea2f99febc40
Skyller <smbaughman08@outlook.com>
2026-02-24 13:43:42 -0500

    added an onclick function to the button, and the same function to js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-const input = document.getElementById("guess").value;
-console.log(input);
+
+function read()
+{
+    const input = document.getElementById("guess").value;
+    console.log(input);
+}
