40f12c1fe4b8b762db97b6e5b4d3eb2130bc3c2a
Skyller <smbaughman08@outlook.com>
2026-02-25 14:24:06 -0500

    added to js, and a little to html

--- index.js
+++ index.js
@@ -7,16 +7,11 @@ function vineBoom() {
         document.getElementById("alert-box").style.transitionDuration = "1s";
         document.getElementById("alert-box").style.opacity = 0;
     },750);
-    /*const audio = new Audio("resources/index/vine-boom.mp3");
-    audio.play();*/
+    const audio = new Audio("resources/index/vine-boom.mp3");
+    audio.play();
 }
 
 
-function read()
-{
-    const input = document.getElementById("guess").value;
-    console.log(input);
-}
 /*
 const phrases = ["the factory must grow", "web programming", "the honored one", "fire and ice", "space elevator", "quantum physics", "hello world", "trains are the best", "binary trees", "afternoon is the best"];
 
@@ -52,11 +47,13 @@ for(let char = 0; char < phrase.length; char++)
 console.log(dashes, "dashes");
 */
 
+const phrases = ["the factory must grow", "web programming", "the honored one", "fire and ice", "space elevator", "quantum physics", "hello world", "trains are the best", "binary trees", "afternoon is the best"];
+const phrase = phrases[Math.floor(Math.random() * 10)];
+console.log(phrase);
+
+/*
 function plaDas() //stands for place dashes
 {
-    const phrases = ["the factory must grow", "web programming", "the honored one", "fire and ice", "space elevator", "quantum physics", "hello world", "trains are the best", "binary trees", "afternoon is the best"];
-    const phrase = phrases[Math.floor(Math.random() * 10)];
-    console.log(phrase);
     const display = document.getElementById("hidden-word");
     const dashes = [];
 
@@ -75,22 +72,45 @@ function plaDas() //stands for place dashes
     }
     console.log(dashes);
 }
+*/
+
+const display = document.getElementById("hidden-word");
+const dashes = [];
+
+for(let lett = 0; lett < phrase.length; lett++)
+{
+    if(phrase[lett] == " ")
+    {
+        display.textContent += " ";
+        dashes.push(" ");
+    }
+    else
+    {
+        dashes.push("_");
+        display.textContent += "_";
+    }
+}
+console.log(dashes);
+
 
 function check()
 {
     const input = document.getElementById("guess").value;
     console.log(input);
 
-    if(phrase.includes(input))
+    for(let char = 0; char < phrase.length; char++)
     {
-        dashes[phrase.indexOf(input)] = input;
-        display.textContent = dashes.join("");
+        if(phrase[char] == input)
+        {
+            dashes[char] = input;
+            display.textContent = dashes.join("");
+        }
+        else
+        {
+            vineBoom();
+        }
     }
 
     console.log(display.textContent);
 }
 
-/*
-<!--This will choose the phrase and place the dashes-->
-        <script>plaDas()</script>
-*/
