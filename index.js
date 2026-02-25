a0855c2446c934d204945e4dd7abba88a439ee99
Skyller <smbaughman08@outlook.com>
2026-02-25 13:43:29 -0500

    renamed the function

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 
-function read()
-{
-    const input = document.getElementById("guess").value;
-    console.log(input);
-}``
-
-function hawktuah() {
+function vineBoom() {
 
     document.getElementById("alert-box").style.transitionDuration = "0s";
     document.getElementById("alert-box").style.opacity = 1;
@@ -17,42 +11,103 @@ function hawktuah() {
     audio.play();*/
 }
 
-const phrases = ["the factory must grow", "web programming", "the honored one", "fire and ice", "space elevator", "quantum physics", "hello world", "trains are the best", "binary trees", "afternoon is the best"];
 
-//const ran = Math.floor(Math.random() * 10);
+function read()
+{
+    const input = document.getElementById("guess").value;
+    console.log(input);
+}
+/*
+const phrases = ["the factory must grow", "web programming", "the honored one", "fire and ice", "space elevator", "quantum physics", "hello world", "trains are the best", "binary trees", "afternoon is the best"];
 
 const phrase = phrases[Math.floor(Math.random() * 10)];
 console.log(phrase);
 console.log(phrase.length);
 const display = document.getElementById("hidden-word");
 console.log(display);
+const dashes = [];
 for(let lett = 0; lett < phrase.length; lett++)
 {
-    /*
-    console.log(phrase[lett]);
-    const disLet = document.createElement("h2");
-    disLet.textContent = phrase[lett]
-    display.appendChild(disLet);
-    const dash = document.createElement("div");
-    display.appendChild(dash);
-    */
     console.log(phrase[lett]);
     if(phrase[lett] == " ")
     {
         console.log("space")
-        display.textContent += " ";
+        dashes.push(" ")
     }
     else
     {
         console.log("blank")
-        display.textContent += "_";
+        dashes.push("_")
     }
 }
-
-console.log(display);
+console.log(dashes);
+display.textContent = dashes.join("");
+console.log(display.textContent);
 
 for(let char = 0; char < phrase.length; char++)
 {
-    console.log(display.textContent[char], "2");
-    display.textContent[char] = phrase[char]
+    dashes[char] = phrase[char];
+    display.textContent = dashes.join("");
+}
+console.log(dashes, "dashes");
+*/
+
+/* //this is the final product, commented so i dont have to change index.html
+function plaDas() //stands for place dashes
+{
+    const phrases = ["the factory must grow", "web programming", "the honored one", "fire and ice", "space elevator", "quantum physics", "hello world", "trains are the best", "binary trees", "afternoon is the best"];
+    const phrase = phrases[Math.floor(Math.random() * 10)];
+    console.log(phrase);
+    const display = document.getElementById("hidden-word");
+    const dashes = [];
+
+    for(let lett = 0; lett < phrase.length; lett++)
+    {
+        if(phrase[lett] == " ")
+        {
+            display.textContent += " ";
+            dashes.push(" ");
+        }
+        else
+        {
+            dashes.push("_");
+            display.textContent += "_";
+        }
+    }
+    console.log(dashes);
+}
+*/
+
+const phrases = ["the factory must grow", "web programming", "the honored one", "fire and ice", "space elevator", "quantum physics", "hello world", "trains are the best", "binary trees", "afternoon is the best"];
+const phrase = phrases[Math.floor(Math.random() * 10)];
+console.log(phrase);
+const display = document.getElementById("hidden-word");
+const dashes = [];
+
+for(let lett = 0; lett < phrase.length; lett++)
+{
+    if(phrase[lett] == " ")
+    {
+        display.textContent += " ";
+        dashes.push(" ");
+    }
+    else
+    {
+        dashes.push("_");
+        display.textContent += "_";
+    }
+}
+console.log(dashes);
+
+function check()
+{
+    console.log("pass")
+}
+
+const input = document.getElementById("guess").value;
+console.log(input);
+
+if(phrase.includes(input))
+{
+    dashes
 }
