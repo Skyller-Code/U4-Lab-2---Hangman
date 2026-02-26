971b1aa545848f58099024312e16879c2527ee6b
Donovan <donosaurusjk@gmail.com>
2026-02-26 13:27:52 -0500

    added loss and win text

--- index.js
+++ index.js
@@ -30,6 +30,22 @@ function decreaseChances() {
     document.getElementById("hanger-img").src = "resources/index/stage" + (6 - parseInt(chances.textContent)) + ".png";
 }
 
+function createWinText() {
+    const winText = document.createElement("div");
+    winText.textContent = "Congratulations, you won! Now you and the hangman can go and live your lives in peace, but the events of this day will be forever scarred into each of you...";
+    winText.classList.add("font");
+    winText.id = "win-text";
+    document.body.appendChild(winText);
+}
+
+function createLoserText() {
+    const loserText = document.createElement("div");
+    loserText.textContent = "You lose. Now you must live the rest of your life with the guilt of killing a man... The phrase was: " + phrase;
+    loserText.classList.add("font");
+    loserText.id = "loser-text";
+    document.body.appendChild(loserText);
+}
+
 const phrases = ["the factory must grow", "web programming", "the honored one", "fire and ice", "space elevator", "quantum physics", "hello world", "trains are the best", "binary trees", "afternoon is the best"];
 const phrase = phrases[Math.floor(Math.random() * 10)];
 console.log(phrase);
@@ -85,8 +101,17 @@ function check() //checks if a guessed letter is in the phrase
 
     document.getElementById("guess").value = "";
 
-    if (!dashes.includes("_") || parseInt(document.getElementById("chances").textContent) <= 0)
+    if (!dashes.includes("_")) //end game if there are no more dashes, meaning the phrase has been fully guessed
     {
+        createWinText();
+        createPlayAgain();
+        document.getElementById("submit").disabled = true;
+        document.getElementById("guess").disabled = true;
+    }
+
+    else if (parseInt(document.getElementById("chances").textContent) <= 0) //end game if there are no more chances, meaning the hanger is fully drawn
+    {
+        createLoserText();
         createPlayAgain();
         document.getElementById("submit").disabled = true;
         document.getElementById("guess").disabled = true;
